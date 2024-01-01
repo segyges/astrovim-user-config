b4d4e34b8a404f0cb6fea60c3f5e4467c532a887
SE Gyges <sanders.aaoron.d@gmail.com>
2024-01-01 00:04:59 -0600

    Add community imports
    Requires golang to be installed, terraform plugin seems to be broken by default.

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,22 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.git.blame-nvim"},
+  { import = "astrocommunity.git.diffview-nvim"},
+  { import = "astrocommunity.pack.bash"},
+  { import = "astrocommunity.pack.docker"},
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown"},
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.remote-development.netman-nvim" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
